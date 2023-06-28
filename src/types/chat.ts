2202da0a3d4c8cabc0a3844232c20d5ec0e15861
Alex Chan <me@iscys.com>
2023-06-28 15:24:24 +0800

    Update chat.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -54,9 +54,10 @@ export interface Folder {
 // // | 'gpt-4-0314'
 // // | 'gpt-4-32k-0314'
 
-export type ModelOptions = 'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k' 
+export type ModelOptions = 'gpt-3.5-turbo' 
   | 'gpt-3.5-turbo-0301' 
   | 'gpt-3.5-turbo-0613' 
+  | 'gpt-3.5-turbo-16k' 
   // | 'gpt-4-0314'
   // | 'gpt-4-32k-0314'
 ;
